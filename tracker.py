885b98e944a68cffb89ca8c6c3afb18a5ebf0fd1
Zayd-Loya <zaidloya@gmail.com>
2024-07-31 08:47:48 +0300

    Updated to use asyncio and aiohttp

--- tracker.py
+++ tracker.py
@@ -1,15 +1,24 @@
 import requests
 import utils
+import asyncio
+import aiohttp
+import time
 
 
-def get_server_spectators(server_url):
-    is_available, result = check_server_availability(server_url)
+async def fetch(session, url):
+    async with session.get(url) as response:
+        response.raise_for_status()
+        return await response.json()
+
+
+async def get_server_spectators(server_url):
+    is_available, result = await check_server_availability(server_url)
     if not is_available:
         return False, result
     else:
         server_data = is_available
 
-    if not check_if_spectators(server_data):
+    if not utils.check_if_spectators(server_data):
         return 0, "The server currently does not have any spectators"
 
     server_name = utils.get_server_name(server_data)
@@ -23,38 +32,34 @@ def get_server_spectators(server_url):
     return {"spectators": spectators, "server_name": server_name}
 
 
-def get_all_spectators():
+async def get_all_spectators():
     servers = utils.load_servers()
-    for server_url in servers:
-        spectators = get_server_spectators(server_url)
+    tasks = [get_server_spectators(server_url) for server_url in servers]
+    results = await asyncio.gather(*tasks)
+    for spectators in results:
         if isinstance(spectators, tuple):
             continue
         else:
             print(spectators)
 
 
-def check_if_spectators(server_json):
-    if server_json.get('message').get('SERVER_INFO').get('slots').get('8').get('current') == 0:
-        return False
-    return True
-
-
-def check_server_availability(server_url):
+async def check_server_availability(server_url):
     if not utils.is_url_valid(server_url):
         return False, "Invalid server URL provided."
     try:
-        response = requests.get(utils.get_server_json_url(server_url))
-        response.raise_for_status()
-        data = response.json()
+        async with aiohttp.ClientSession() as session:
+            data = await fetch(session, utils.get_server_json_url(server_url))
 
         if data.get('type') == 'error':
             return False, "Server can not be found"
 
         return data, "Server found successfully"
-    except requests.exceptions.RequestException as e:
+    except aiohttp.ClientError as e:
         return False, f"Request error: {str(e)}"
     except ValueError:
         return False, "Invalid JSON response from server"
 
 
-get_server_spectators("https://battlelog.battlefield.com/bf4/servers/show/pc/c067dded-25ad-4b31-b1c2-f44072c635d0/bZ1-HARDCORE-ALL-MAPS-BANZORE-COM/")
+time1 = time.time()
+asyncio.run(get_all_spectators())
+print(time.time() - time1)

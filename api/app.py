6303914ba6447253cd8f85306cda7928e92a9445
Zayd-Loya <zaidloya@gmail.com>
2024-08-05 08:19:43 +0300

    Testing routes

--- api/app.py
+++ api/app.py
@@ -12,24 +12,36 @@ def index():
 
 @app.route('/api/track_player', methods=['POST'])
 def track_player_route():
-    data = request.json
-    input_value = data.get('input_value')
-    result = asyncio.run(track_player(input_value))
-    return jsonify(result)
+    try:
+        data = request.json
+        input_value = data.get('input_value')
+        result = asyncio.run(track_player(input_value))
+        return jsonify(result)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/api/get_server_spectators', methods=['POST'])
 def get_server_spectators_route():
-    data = request.json
-    server_url = data.get('server_url')
-    result = asyncio.run(get_server_spectators(server_url))
-    return jsonify(result)
+    try:
+        data = request.json
+        server_url = data.get('server_url')
+        result = asyncio.run(get_server_spectators(server_url))
+        return jsonify(result)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/api/get_all_spectators', methods=['POST'])
 def get_all_spectators_route():
-    result = asyncio.run(get_all_spectators())
-    return jsonify(result)
+    try:
+        result = asyncio.run(get_all_spectators())
+        return jsonify(result)
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
